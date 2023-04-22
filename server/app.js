930b42989e63fa7d0a67a06f288e2d02715272aa
Avadhoot05 <khedekaravadhoot@gmail.com>
2023-04-23 00:14:02 +0530

    setup client server comm

--- server/app.js
+++ server/app.js
@@ -1,38 +1,50 @@
 // import ffmpeg from 'fluent-ffmpeg';
+import express from 'express';
 
-// const app = express();
-// app.use(express.json());
+const app = express();
+app.use(express.json());
+
+const gotRequest =  (req,res) => {
+    console.log("got req");
+    if(req)
+        console.log(req.body);
+    else
+        console.log("no body")
+}
 
 
+const router = express.Router();
+router.post('/upload', gotRequest);
+app.use('/', router);
 
 
-// const port =  3000;
-// app.listen(port, () => {
-//     console.log(`Server is running at ${port}`);
-//   });
+const port =  3000;
+app.listen(port, () => {
+    console.log(`Server is running at ${port}`);
+  });
 
 
-(function() {
-    const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-    console.log(ffmpegPath);
-    const ffmpeg = require('fluent-ffmpeg');
-    ffmpeg.setFfmpegPath(ffmpegPath);
+// (function() {
+//     const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+//     console.log(ffmpegPath);
+//     const ffmpeg = require('fluent-ffmpeg');
+//     ffmpeg.setFfmpegPath(ffmpegPath);
 
-    // ffmpeg("vid.mp4")
-    // .output(`OUTPUT.mp4`)
-    // .videoCodec("libx264")
-    // .size("1280x720")
-    // .on("error", function(err){
-    //     console.log(err);
-    // })
-    // .run();
+//     // ffmpeg("vid.mp4")
+//     // .output(`OUTPUT.mp4`)
+//     // .videoCodec("libx264")
+//     // .size("1280x720")
+//     // .on("error", function(err){
+//     //     console.log(err);
+//     // })
+//     // .run();
 
-    ffmpeg("vid.mp4")
-    .output(`OUTPUTwith15FPS.mp4`)
-    .videoCodec("libx264")
-    .fps(500)
-    .on("error", function(err){
-        console.log(err);
-    })
-    .run();
-})();
+//     ffmpeg("vid.mp4")
+//     .output(`OUTPUTwith15FPS.mp4`)
+//     .videoCodec("libx264")
+//     .fps(500)
+//     .on("error", function(err){
+//         console.log(err);
+//     })
+//     .run();
+// })();
